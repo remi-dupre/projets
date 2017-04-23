53e16507817f9b29938bca8b6c4ae68f514aae8e
Rémi Dupré <remim.dupre@gmail.com>
2017-04-23 12:07:25 +0200

    Ajout du sujet de dm et du code de base

---
+++ probas/dm.py
@@ -0,0 +1,27 @@
+import numpy as np
+from numpy.random import rand
+from scipy.stats import norm, chi2
+
+author = "Ici votre nom/prénom"
+ 
+def gaussian_ci_nu(samples_x, alpha):
+    return [-1, -1] #TODO
+ 
+def chi2_noncentral_df1_ci_lambda(samples_y, alpha):
+    return [-1, -1] #TODO
+ 
+def chi2_noncentral_dfp_ci_lambda(sample_z, p, alpha):
+    return [-1, -1] #TODO
+ 
+ 
+def rejection_sampling(f, g, g_sampler, c, n):
+    return [] #TODO
+
+def truncated_gaussian_sampling(mu, sigma, n, a):
+    return [] #TODO
+
+def truncated_gaussian_sampling_simple(mu, sigma, n, a):
+    return [] #TODO
+
+def test_inequalities():
+    return #TODO
