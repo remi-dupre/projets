14020a722ae92b096a943660795450be90d81403
Rémi Dupré <remim.dupre@gmail.com>
2017-04-24 22:11:56 +0200

    L'algo de tirage sur une loi normale tronquée, simplifié

--- probas/dm.py
+++ probas/dm.py
@@ -19,7 +19,7 @@ quartile_N0 = norm.ppf
 # ----- Question 2.1 -----
 
 def gaussian_ci_nu(samples_x, alpha):
-    # On a trouvé un intervale de confiance sous la forme :
+    # On a trouvé un intervalle de confiance sous la forme :
     #   moyenne +- a / sqrt(n)
     n = len(samples_x)
     a = quartile_N0(1 - alpha/2)
@@ -28,7 +28,7 @@ def gaussian_ci_nu(samples_x, alpha):
     return [x-d, x+d]
 
 def test_gaussian_ci_nu(n = 100, alpha = 0.05) :
-    # Cherche 1000 fois l'intervale de confiance pour des nouvelles valeures des x_1 ... x_n
+    # Cherche 1000 fois l'intervalle de confiance pour des nouvelles valeurs des x_1 ... x_n
     # On compte le nombre de fois où mu est dedans
     N = norm(42, 1)
     
@@ -55,7 +55,7 @@ def chi2_noncentral_df1_ci_lambda(samples_y, alpha):
     return [0, (x+d)**2]
     
 def test_chi2_noncentral_df1_ci_lambda(n = 100, cent = 0.5, alpha = 0.05) :
-    # Cherche 1000 fois l'intervale de confiance pour des nouvelles valeures des x_1 ... x_n
+    # Cherche 1000 fois l'intervale de confiance pour des nouvelles valeurs des x_1 ... x_n
     # On compte le nombre de fois où lambda est dedans
     N = norm(-np.sqrt(cent), 1) # lambda = cent
     
@@ -100,24 +100,65 @@ def test_trucated_gaussian_pdf(mu = 42, sigma = 4.2, a = 53) :
     plt.plot(vals_x, vals_y)
     plt.show()
 
-test_trucated_gaussian_pdf()
-
 # ----- Question 3.3 -----
 
 def rejection_sampling(f, g, g_sampler, c, n):
+    # Implémentation de l'algorithme décrit dans le sujet
     ret = [] # Les valeurs correspondant à notre tirage aléatoire
     while len(ret) < n :
         x = g_sampler()
         u = uniform(0, 1).rvs()
         if u <= f(x) / (c*g(x)) :
-            ret.push_back(x)
+            ret.append(x)
     return ret
 
 def truncated_gaussian_sampling(mu, sigma, n, a):
-    return [] #TODO
+    f = truncated_gaussian_pdf(mu, sigma, a)
+    g = norm(mu, sigma).pdf
+    g_sampler = norm(mu, sigma).rvs
+    c = 1 / norm(mu, sigma).cdf(a) # Dans notre cas la constante c optimale est K
+    return rejection_sampling(f, g, g_sampler, c, n)
+
+def test_truncated_gaussian_sampling(mu = 42, sigma = 4.2, n = 10**3, a = 50) :
+    g, d = mu - 5*sigma, mu + 5*sigma
+
+    # Trace la densité de la gaussienne tronquée
+    sample = truncated_gaussian_sampling(mu, sigma, n, a)
+    plt.hist(sample, 20, normed=1/n)
+
+    # Trace la répartition d'un tirage
+    f = truncated_gaussian_pdf(mu, sigma, a)
+    vals_x = np.linspace(g, d, 1000)
+    vals_y = [ f(x) for x in vals_x ]
+    plt.plot(vals_x, vals_y)
+    
+    plt.show()
+
+# ----- Question 3.4 -----
 
 def truncated_gaussian_sampling_simple(mu, sigma, n, a):
-    return [] #TODO
+    # Un algorithme plus naïf pour la loi uniforme tronquée
+    ret = [] # Les valeurs correspondant à notre tirage aléatoire
+    while len(ret) < n :
+        x = norm(mu, sigma)
+        if x <= a :
+            ret.append(x)
+    return ret
+
+def test_truncated_gaussian_sampling_simple(mu = 42, sigma = 4.2, n = 10**3, a = 50) :
+    g, d = mu - 5*sigma, mu + 5*sigma
+
+    # Trace la densité de la gaussienne tronquée
+    sample = truncated_gaussian_sampling(mu, sigma, n, a)
+    plt.hist(sample, 20, normed=1/n)
+
+    # Trace la répartition d'un tirage
+    f = truncated_gaussian_pdf(mu, sigma, a)
+    vals_x = np.linspace(g, d, 1000)
+    vals_y = [ f(x) for x in vals_x ]
+    plt.plot(vals_x, vals_y)
+    
+    plt.show()
 
 def test_inequalities():
     return #TODO
